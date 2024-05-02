06a5b0580bd21caff273518e31be609cb65ff028
Ruby <pineapples1104@gmail.com>
2024-05-02 13:47:48 -0600

    DOM manip assignment

---
+++ DOM amnipulation/dom-basics.js
@@ -0,0 +1,17 @@
+let fullName = 'Ruby Larson';
+const h1 = document.querySelector('h1');
+h1.innerHTML += '<em>' + fullName + '</em>';
+
+const section = document.createElement('section');
+const h2 = document.createElement('h2');
+h2.textContent = 'Section 3';
+const p = document.createElement('p');
+p.textContent = 'This is a paragraph in section 3';
+
+section.appendChild(h2);
+section.appendChild(p);
+document.body.appendChild(section);
+
+
+
+// document.querySelector('h1').innerHTML = `<em>${fullName}</em>`;
