ecef747686cfc723b79a917816dd67c39f9f24a5
Ruby <pineapples1104@gmail.com>
2024-05-04 16:57:10 -0600

    Light/Dark mode for mission

---
+++ mission/mission.js
@@ -0,0 +1,18 @@
+const mySelect = document.getElementsByTagName('select')[0];
+function changeTheme()
+{
+    console.log(mySelect.value)
+
+    if (mySelect.value == 'dark')
+        {
+            document.body.classList.add('dark');
+            document.querySelector('.logo').src = 'byui-logo_white.png';
+        }
+        else
+        {
+            document.body.classList.remove('dark');
+            document.querySelector('.logo').src = 'byui-logo_blue.webp';
+        }
+}
+    
+mySelect.addEventListener('click', changeTheme)
