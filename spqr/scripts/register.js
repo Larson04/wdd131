19e5a3698c93dabd05d37a1301755bf83c471771
Ruby <pineapples1104@gmail.com>
2024-07-17 18:04:50 -0600

    Everything but register menu button is done

--- spqr/scripts/register.js
+++ spqr/scripts/register.js
@@ -1,3 +1,28 @@
+// const menuButton = document.querySelector('.logo button');
+// // menuButton.classList.add('hide')
+// menuButton.addEventListener("click", toggleMenu);
+
+// function toggleMenu() {
+//     const menu = document.querySelector("#menu");
+//     // menu.classList.remove(".hide");
+
+//     if (menu.classList.contains("hide")) {
+//       menu.classList.remove("hide");
+//     } else {
+//       menu.classList.add("hide");
+//     }
+//   }
+
+// function handleResize(){
+//     const menu = document.querySelector("#menu");
+//     if (window.innerWidth > 1000) {
+//         menu.classList.remove("hide");
+//     } else {
+//         menu.classList.add("hide");
+//     }
+  
+// }
+
 document.addEventListener('DOMContentLoaded', () => {
     let participantCount = 1;
   
